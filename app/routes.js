e1a1e76a72773d3c8ec13ace1b6e58aa0afe567f
Krzysztof Jendrzyca <kjendrzyca@future-processing.com>
2015-07-13 18:30:43 +0200

    adding generic GET route for testing purposes

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,10 @@ import logger from './logger';
 
 let configure = function(app) {
 
+    app.get('/', (req, res) => {
+        res.end("hello from Klesk");
+    });
+
     app.post('/', (req, res) => {
         logger(req.body);
         res.end("congratulation it works !");
