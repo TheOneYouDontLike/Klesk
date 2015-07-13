ba31d6fb7a6c16d385f6f7cc7589a5f8bd1be619
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-07-13 18:25:41 +0200

    removing build folder from gitignore

---
+++ build/server.js
@@ -0,0 +1,156 @@
+require("source-map-support").install();
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+	
+	var _config = __webpack_require__(1);
+	
+	var _config2 = _interopRequireDefault(_config);
+	
+	var _appLogger = __webpack_require__(5);
+	
+	var _appLogger2 = _interopRequireDefault(_appLogger);
+	
+	var _appRoutes = __webpack_require__(6);
+	
+	var _appRoutes2 = _interopRequireDefault(_appRoutes);
+	
+	var _express = __webpack_require__(3);
+	
+	var _express2 = _interopRequireDefault(_express);
+	
+	var _bodyParser = __webpack_require__(4);
+	
+	var _bodyParser2 = _interopRequireDefault(_bodyParser);
+	
+	var app = (0, _express2['default'])();
+	app.use(_bodyParser2['default'].json());
+	
+	_appRoutes2['default'].configure(app);
+	
+	var port = _config2['default'].port;
+	app.listen(port);
+	
+	(0, _appLogger2['default'])('Listening on port ' + port);
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	exports["default"] = {
+	    port: 1666
+	};
+	module.exports = exports["default"];
+
+/***/ },
+/* 2 */,
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = require("express");
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = require("body-parser");
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, '__esModule', {
+	    value: true
+	});
+	
+	exports['default'] = function (message) {
+	    console.log(message);
+	};
+	
+	module.exports = exports['default'];
+
+/***/ },
+/* 6 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, '__esModule', {
+	    value: true
+	});
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+	
+	var _logger = __webpack_require__(5);
+	
+	var _logger2 = _interopRequireDefault(_logger);
+	
+	var configure = function configure(app) {
+	
+	    app.post('/', function (req, res) {
+	        (0, _logger2['default'])(req.body);
+	        res.end('congratulation it works !');
+	    });
+	};
+	
+	exports['default'] = {
+	    configure: configure
+	};
+	module.exports = exports['default'];
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=server.js.map
