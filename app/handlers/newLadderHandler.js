8ff7bf5eeae23bc92348de5a56156b49e1542c1e
tarnas14 <tarnas14@gmail.com>
2015-07-17 00:48:25 +0200

    adding validation preventing multiple ladders with the same name

--- app/handlers/newLadderHandler.js
+++ app/handlers/newLadderHandler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import _ from 'lodash';
+
 function Ladder(name) {
     return {
         name: name,
@@ -8,16 +10,32 @@ function Ladder(name) {
 }
 
 let newLadderHandler = function(persistence) {
+
+    function _prepareLadderExistsErrorMessage(ladderName) {
+        return 'Ladder `' + ladderName + '` already exists.';
+    }
+
     return {
         makeItSo(parsedCommand, callback) {
             let ladderName = parsedCommand.arguments[1];
 
-            persistence.add(Ladder(ladderName), (error) => {
+            persistence.getAll(function(error, data) {
                 if (error) {
                     callback(error.message, null);
                 }
 
-                callback(null, 'Created new ladder: ' + ladderName);
+                let ladderAlreadyExists = _.any(data, { name: ladderName });
+                if (ladderAlreadyExists) {
+                    callback(null, _prepareLadderExistsErrorMessage(ladderName));
+                } else {
+                    persistence.add(Ladder(ladderName), (error) => {
+                        if (error) {
+                            callback(error.message, null);
+                        }
+
+                        callback(null, 'Created new ladder: ' + ladderName);
+                    });
+                }
             });
         }
     };
