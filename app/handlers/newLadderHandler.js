955e64876bc3a6edd8a43c018ed73d7db6268d50
tarnas14 <tarnas14@gmail.com>
2015-07-17 08:11:23 +0200

    spaghetti fix for newLadder callbacks and joinLadder

--- app/handlers/newLadderHandler.js
+++ app/handlers/newLadderHandler.js
@@ -19,23 +19,26 @@ let newLadderHandler = function(persistence) {
         makeItSo(parsedCommand, callback) {
             let ladderName = parsedCommand.arguments[1];
 
-            persistence.getAll(function(error, data) {
+            persistence.getAll((error, data) => {
                 if (error) {
                     callback(error.message, null);
+                    return;
                 }
 
                 let ladderAlreadyExists = _.any(data, { name: ladderName });
                 if (ladderAlreadyExists) {
                     callback(null, _prepareLadderExistsErrorMessage(ladderName));
-                } else {
-                    persistence.add(Ladder(ladderName), (error) => {
-                        if (error) {
-                            callback(error.message, null);
-                        }
-
-                        callback(null, 'Created new ladder: ' + ladderName);
-                    });
+                    return;
                 }
+
+                persistence.add(Ladder(ladderName), (error) => {
+                    if (error) {
+                        callback(error.message, null);
+                        return;
+                    }
+
+                    callback(null, 'Created new ladder: ' + ladderName);
+                });
             });
         }
     };
