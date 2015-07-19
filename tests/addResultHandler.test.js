33f9455552282cab89d20a93ffbd6856f550f4fc
tarnas14 <tarnas14@gmail.com>
2015-07-19 13:39:55 +0200

    used proper, working sinon.spy syntax within a beforeEach hook

--- tests/addResultHandler.test.js
+++ tests/addResultHandler.test.js
@@ -4,23 +4,31 @@ import assert from 'assertthat';
 import sinon from 'sinon';
 import addResultHandler from '../app/handlers/addResultHandler';
 
-let ladder = 
-{
-    name: 'laddername', 
-    matches: [{player1: 'winner', player2: 'loser', winner: ''}]
-};
-
-let fakePersistence = {
-    update(query, update, callback) {
-        query(ladder); update(ladder); callback();
-    }
-};
-let updateSpy = sinon.spy(fakePersistence.update);
-
-let handler = addResultHandler(fakePersistence);
-let callbackSpy = sinon.spy();
+let ladder = {};
+let fakePersistence = {};
+let updateSpy = {};
+let handler = {};
+let callbackSpy = {};
 
 describe('addResultHandler', () => {
+    beforeEach(() => {
+        ladder = 
+        {
+            name: 'laddername', 
+            matches: [{player1: 'winner', player2: 'loser', winner: ''}]
+        };
+
+        fakePersistence = {
+            update(query, update, callback) {
+                query(ladder); update(ladder); callback();
+            }
+        };
+        updateSpy = sinon.spy(fakePersistence, "update");
+
+        handler = addResultHandler(fakePersistence);
+        callbackSpy = sinon.spy();
+    });
+
     it('should save result of the match', () => {
         //given
         let parsedCommand = {
@@ -32,6 +40,7 @@ describe('addResultHandler', () => {
         handler.makeItSo(parsedCommand, callbackSpy);
 
         //then
+        assert.that(updateSpy.called).is.true();
         assert.that(callbackSpy.calledWith(null, 'Result saved!')).is.true();
     });
 
@@ -66,7 +75,6 @@ describe('addResultHandler', () => {
         handler.makeItSo(parsedCommand, callbackSpy);
 
         //then
-        assert.that(updateSpy.called).is.false();
         assert.that(callbackSpy.calledWith(null, 'This match result has already been added.')).is.true();
     });
 
