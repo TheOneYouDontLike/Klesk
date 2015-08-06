ab0663d1e54977517210ce47aad94a9447410e0b
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-08-06 22:17:10 +0200

    changing Logger to log error to file if specified in config - draft / #22

--- app/logger.js
+++ app/logger.js
@@ -1,5 +1,23 @@
 'use strict';
 
+import fs from 'fs';
+import path from 'path';
+import config from '../config';
+
 export default function(message) {
     console.log(message);
+
+    if (!config.logErrorsToFile) {
+        return;
+    }
+    let timestamp = 'log' + new Date().toString();
+    let logFilePath = path.resolve(config.errorsLogPath, timestamp);
+
+    let errorMessage = {
+        message: message
+    };
+
+    fs.writeFile(logFilePath, JSON.stringify(errorMessage), (error) => {
+        console.log(error);
+    });
 }
