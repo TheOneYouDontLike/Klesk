79d2c9a61e6e043c70b0a7bdd6796684c26316fd
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-08-09 22:11:35 +0200

    logger logs strings, objects and errors / #22

--- app/logger.js
+++ app/logger.js
@@ -4,21 +4,43 @@ import fs from 'fs';
 import path from 'path';
 import config from '../config';
 
-export default function(stuff) {
-    console.log('log: ', stuff);
+export default function(stuffToLog) {
+    console.log('log: ', stuffToLog);
 
     if (!config.logErrorsToFile) {
         return;
     }
 
+    if (_isError(stuffToLog)) {
+        let thingToLog = {
+            message: stuffToLog.message,
+            stack: stuffToLog.stack
+        };
+
+        _logToFile(thingToLog);
+    }
+
+    let thingToLog = {
+        message: stuffToLog
+    };
+
+    _logToFile(thingToLog);
+}
+
+function _isError(stuffToLog) {
+    let isAnObject = typeof stuffToLog === 'object';
+    let hasErrorProperties = stuffToLog.hasOwnProperty('message') && stuffToLog.hasOwnProperty('stack');
+
+    return isAnObject && hasErrorProperties;
+}
+
+function _logToFile(thingToLog) {
     let timestamp = 'log_' + Date.now().toString();
     let logFilePath = path.resolve(config.errorsLogPath, timestamp);
 
-    let thingToLog = {
-        stuff: stuff
-    };
-
     fs.writeFile(logFilePath, JSON.stringify(thingToLog), (error) => {
-        if (error) console.log('error: ', error);
+        if (error) {
+            console.log('error: ', error);
+        }
     });
 }
