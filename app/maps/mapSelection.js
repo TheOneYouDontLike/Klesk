29bd985dd545bc5c08b6fce0b67f37123c711c73
tarnas14 <tarnas14@gmail.com>
2015-08-23 22:05:09 +0200

    implemented map selection module biased towards maps with higher keyword counts; ignoring negative keyword vote values

--- app/maps/mapSelection.js
+++ app/maps/mapSelection.js
@@ -1,17 +1,56 @@
 'use strict';
 
+import _ from 'lodash';
+
+function _getMapListFavouringUpvotedMaps(maps) {
+    let biasedMapList = [];
+
+    _.forEach(maps, (map) => {
+        if (map.votes.keyword < 0) {
+            return;
+        }
+
+        for(let i = 0; i < map.votes.keyword + 1; ++i) {
+            biasedMapList.push(map);
+        }
+    });
+
+    biasedMapList = _.shuffle(biasedMapList);
+
+    return biasedMapList;
+}
+
+function _initializeVotesForMapsWithoutKeywordVote(maps, keyword) {
+    let keywordVoteMap =_.map(maps, (map) => {
+        if (_.has(map.votes, keyword)) {
+            return map;
+        }
+
+        map.votes.keyword = 0;
+        return map;
+    });
+
+    return keywordVoteMap;
+}
+
 let mapSelection = function() {
     var randoms = {
-        random() {
-            return Math.random();
+        getRandomElement(collection) {
+            return collection[Math.floor(Math.random()*collection.length)];
         }
     };
 
     return { 
-        getMapFrom(maps, keywords) {
-            if(!keywords) {
-                return maps[Math.floor(randoms.random()*maps.length)];
+        getMapFrom(maps, keyword) {
+            if(!keyword) {
+                return randoms.getRandomElement(maps);
             }
+
+            let mapsWithKeywordVotes = _initializeVotesForMapsWithoutKeywordVote(maps, keyword);
+
+            let mapsToSelectFrom = _getMapListFavouringUpvotedMaps(mapsWithKeywordVotes);
+
+            return randoms.getRandomElement(mapsToSelectFrom);
         }
     };
 };
